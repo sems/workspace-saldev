2bc8e52a9564485615cb068808271d858583cd80
semspanhaak <sspanhaa8952@student.landstede.nl>
2015-11-25 13:56:11 +0100

    Moderizer weg gehaalt

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-$(document).ready(function() {
-	show();
+/**
+Get Default empty todo-list object.
+@return object empty todo-list.
+*/
+var getDefaultObject = function() {
+    return {
+        list: [],
+        id: 0,
+		prio: 0,
+    };
 
-	//Maakt verbinding bij een click van de button
-	$("#opslaan").click(opslaan);
-	$('#clear').click(verwijderen);
-	
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
