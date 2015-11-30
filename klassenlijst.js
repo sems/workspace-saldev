57f9196a2f9d619b59856206f852b71021e3c7f6
semspanhaak <sspanhaa8952@student.landstede.nl>
2015-11-30 11:25:21 +0100

    Klassenlijst nu met opmaak

--- klassenlijst.js
+++ klassenlijst.js
@@ -5,15 +5,15 @@
 		var $element = $('#klassenlijst');
 		
 		if(response.code === 0) {
-			// laat de ontvangen lijst zien en sla op.
+			// Hij laat de ontvangen lijst zien en gaat hem opslaan
 			for(var i = 0; i < response.data.length; i++) {
-				$element.append('<a href="' + response.data[i].url + '" target="_blank"><img src="' + response.data[i].img + '" alt="' + response.data[i].subdomain + '" /><br />' + response.data[i].subdomain + '</a>');
+				$element.append('<a href="' + response.data[i].url + '" target="_blank"><img src="' + response.data[i].img + '" alt="' + response.data[i].subdomain + '" />' + "</br>" + response.data[i].subdomain + '</a>' + "</br>");
 			}
 		} else {
-			alert('Fout! ' + response.error);
+			alert('Er ging iets fout! ' + response.error);
 		}
     }).fail(function(xhr, errorState, errorMessage) {
-        // laat je hier de opgeslagen lijst zien of een fout
-		alert('Fout! ' + errorMessage);
+        // Hier laat je  de opgeslagen lijst zien of een fout
+		alert('Er ging iets fout! ' + errorMessage);
     });
 });
