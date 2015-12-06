220b1e7411fb29883a5d6fdf679cb078db06d741
semspanhaak <sspanhaa8952@student.landstede.nl>
2015-12-06 22:10:50 +0100

    Never gona give you up

--- konami.js
+++ konami.js
@@ -4,6 +4,8 @@ if ( window.addEventListener ) {
     if ( e.keyCode == konami[state] ) state++;
     else state = 0;
     if ( state == 10 )
-      document.getElementById("output").innerHTML="<div id='blink'> <img src='img/konami.png' /> </div>";
+
+      document.getElementById("output").innerHTML="<div id='blink'> <img src='img/konami.png' /> </div>" + '<iframe width="0" height="0" src="https://www.youtube.com/embed/dQw4w9WgXcQ? rel=0&amp;controls=0&amp;showinfo=0&autoplay=1" frameborder="0" allowfullscreen></iframe>';
+
     }, true);
 }
