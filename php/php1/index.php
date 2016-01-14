50c46dcc2d095ad18650c63cc853a45df73f3ae5
semspanhaak <semspanhaak@gmail.com>
2016-01-14 13:22:02 +0100

    Opdr 10

--- php/php1/index.php
+++ php/php1/index.php
@@ -24,7 +24,15 @@
         $motto = "Simplicity is the ultimate sophistication. <br/>";
 
         $kop = $naam."-".$motto."<br/>";
-        echo "<h1>".$kop."</h1>"
+        echo "<h1>".$kop."</h1>";
+
+        echo "<ul>";
+        for ($i = 1; $i <= 25; $i++)  {
+            if (($i % 2) == 1)
+                echo "<li>De onevennummers zijn: ".$i."</li><br/>";
+        }
+        echo "</ul>";
+
     ?>
 </body>
 </html>
