b1d8591b9e5b3f5ff7e2431aa1f0a839551811f6
semspanhaak <semspanhaak@gmail.com>
2016-01-14 11:35:20 +0100

    Opdr t/m 7

--- php/php1/index.php
+++ php/php1/index.php
@@ -6,6 +6,23 @@
 </head>
 <body>
     <?php
+        $naam = "Sem";
+        echo $naam. "<br/>";
+        echo "De lengte van naam: ".strlen($naam). "<br/>";
+
+        $getal1 = 4;
+        $getal2 = 7;
+        $getal3 = 2;
+
+        $som = $getal1 + $getal2 + $getal3;
+
+        echo " De som is: " . $som. "<br/>";
+
+        $gemiddelde = $som / 3;
+        echo "Het gemiddelde is: " .$gemiddelde. "<br/>";
+
+        $motto = "Simplicity is the ultimate sophistication."
+
     ?>
 </body>
 </html>
