6b708dffb068e8c418ab801273f7ede0b062ca43
Sem Spanhaak <sspanhaa8952@student.landstede.nl>
2016-09-18 22:19:42 +0200

    Update the online credentials

--- inc/config.php
+++ inc/config.php
@@ -1,13 +1,13 @@
 <?php
     session_start();
     /*
-        pwd: HaTLmmoRHVPzPBwN
+        pwd: NtxeTVL6RCGtWN82
     */
     //database credentials
     define('DBHOST','localhost');
-    define('DBNAME','workspace');
-    define('DBUSER','root');
-    define('DBPASS','');
+    define('DBNAME','studentdb_sspanhaak');
+    define('DBUSER','sspanhaak');
+    define('DBPASS','NtxeTVL6RCGtWN82');
 
     $db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
