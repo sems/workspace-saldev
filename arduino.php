0ef86e86025885d1a1e13f4c134a41cfc2daa8ee
Sem Spanhaak <semspanhaak@gmail.com>
2017-02-07 11:05:06 +0100

    Arduino fix

---
+++ arduino.php
@@ -0,0 +1,10 @@
+<?php
+    require_once "inc/package.inc.php";
+    require("inc/config.php");
+
+    $view = "views/arduino.php";
+    $sectionActive = "arduino";
+
+    include_once $template;
+?>
+ 
