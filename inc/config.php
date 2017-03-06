44c38f6d2427dfcc750e4a432c70057509ea6140
Sem Spanhaak <semspanhaak@gmail.com>
2017-03-06 16:09:45 +0100

    Config

--- inc/config.php
+++ inc/config.php
@@ -1,16 +1,16 @@
 <?php
     session_start();
 
-    define('DBHOST','localhost');
-    define('DBNAME','studentdb_sspanhaak');
-    define('DBUSER','sspanhaak');
-    define('DBPASS','NtxeTVL6RCGtWN82');
+    // define('DBHOST','localhost');
+    // define('DBNAME','u681724605_work');
+    // define('DBUSER','u681724605_admin');
+    // define('DBPASS','xyVwHU5y8zA0L');
 
     //database credentials
-    // define('DBHOST','localhost');
-    // define('DBNAME','workspace');
-    // define('DBUSER','root');
-    // define('DBPASS','');
+    define('DBHOST','localhost');
+    define('DBNAME','workspace');
+    define('DBUSER','root');
+    define('DBPASS','');
 
     $db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
